c296a0a9ab1f7cbe2143013c05c62cac0ec25e57
Yeonjoo Lee <121855519+Lee-Yeonjoo@users.noreply.github.com>
2024-12-04 16:44:16 +0900

    [Silver I] Title: 포도주 시식, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,19 +1,18 @@
 import sys
-from functools import cache
 input = sys.stdin.readline
 
 n = int(input().rstrip())
-dp = [[0] * n for _ in range(3)]  #3가지 경우를 저장
-x = int(input().rstrip())
-dp[0][0] = x
-dp[1][0] = x
-dp[2][0] = 0
+wine = [0] * 10001
+for i in range(n):
+    wine[i] = int(input().rstrip())
 
-for i in range(1, n):
-    x = int(input().rstrip())
+dp = [0] * 10001
+dp[0] = wine[0]
+dp[1] = dp[0] + wine[1]
+dp[2] = max(dp[1], dp[0] + wine[2], wine[1] + wine[2])
 
-    dp[0][i] = dp[1][i-1] + x
-    dp[1][i] = dp[2][i-1] + x
-    dp[2][i] = max([dp[0][i-1], dp[1][i-1], dp[2][i-1]])
+for i in range(3, n):
+    #max{자신을 포함x, 자신을 포함하는데 이전꺼x, 자신 포함하는데 이전꺼 o)
+    dp[i] = max(dp[i-1], dp[i-2] + wine[i], dp[i-3] + wine[i-1] + wine[i])
 
-print(max([dp[0][n-1], dp[1][n-1], dp[2][n-1]]))
+print(dp[n-1])
