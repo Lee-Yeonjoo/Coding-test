20a52f06a104fb71502fcfd1c31a1a4e1cb0945c
Yeonjoo Lee <121855519+Lee-Yeonjoo@users.noreply.github.com>
2025-01-09 19:24:46 +0900

    [Gold V] Title: 로봇 청소기, Time: 40 ms, Memory: 32488 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,3 +1,4 @@
+#반시계 방향으로 좌표 찾는 로직을 연산을 통해 간단히 한 코드
 import sys
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
@@ -8,81 +9,42 @@ room = []
 for _ in range(N):
     room.append(list(map(int, input().split())))
 
-#d가 북쪽일 때 방향
-dx_N = [0, 1, 0, -1]
-dy_N = [-1, 0, 1, 0]
-d_N = [3, 2, 1, 0]
-
-#d가 동쪽일 때 방향
-dx_E = [-1, 0, 1, 0]
-dy_E = [0, -1, 0, 1]
-d_E = [0, 3, 2, 1]
-
-#d가 남쪽일 때 방향
-dx_S = [0, -1, 0, 1]
-dy_S = [1, 0, -1, 0]
-d_S = [1, 0, 3, 2]
-
-#d가 서쪽일 때 방향
-dx_W = [1, 0, -1, 0]
-dy_W = [0, 1, 0, -1]
-d_W = [2, 1, 0, 3]
-
+#북동남서(0,1,2,3)순서로 설정 
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
 count = 0
-nd = 0
 def dfs(x, y, d):
     global count
-    global nd
 
-    if room[x][y] == 0:
+    if room[x][y] == 0:  #후진하면서 다시 방문할 수도 있어서 조건문 필요
         room[x][y] = 2  #현재 칸 청소
         count += 1
 
+    #반시계 방향으로 청소 안한 칸 탐색
     for i in range(4):
-        if d == 0:
-            nx = x + dx_N[i]
-            ny = y + dy_N[i]
-            nd = d_N[i]
-        
-        if d == 1:
-            nx = x + dx_E[i]
-            ny = y + dy_E[i]
-            nd = d_E[i]
-        
-        if d == 2:
-            nx = x + dx_S[i]
-            ny = y + dy_S[i]
-            nd = d_S[i]
-
-        if d == 3:
-            nx = x + dx_W[i]
-            ny = y + dy_W[i]
-            nd = d_W[i]
+        nd = (d + 3) % 4  #왼쪽으로 90도 회전하는 식
+        nx = x + dx[nd]
+        ny = y + dy[nd]
             
         if room[nx][ny] == 0:
             dfs(nx, ny, nd)
+            #중요!! 여기서 리턴을 꼭 해줘야 한다. 한번 dfs해준 후 다른 방향에서 다시 dfs하지 않고 그냥 손절.
             return
+        
+        #현재 방향을 돌린 방향으로
+        d = nd
     
     #주변을 다 봤는데 청소하지 않은 빈칸이 없는 경우
     #각 방향에 대한 bx,by는 후진했을 때의 좌표
-    if nd == 0:
-        bx = x + dx_N[1]
-        by = y + dy_N[1]
-    if nd == 1:
-        bx = x + dx_E[1]
-        by = y + dy_E[1]
-    if nd == 2:
-        bx = x + dx_S[1]
-        by = y + dy_S[1]
-    if nd == 3:
-        bx = x + dx_W[1]
-        by = y + dy_W[1]
+    nd = (d+2) % 4  #현재 방향과 반대(후진 방향) 방향을 구하는 식.
+    bx = x + dx[nd]
+    by = y + dy[nd]
     
     #후진했을 때 벽이면 리턴
     if room[bx][by] == 1:
         return
 
-    #후진했을 때 벽이 아니므로 탐색색
+    #후진했을 때 벽이 아니므로 탐색
     dfs(bx, by, d)
 
 dfs(r, c, d)
