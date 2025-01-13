5dfead599930d04952613220d0810c45e4a1869c
Yeonjoo Lee <121855519+Lee-Yeonjoo@users.noreply.github.com>
2025-01-13 15:07:09 +0900

    [Silver IV] Title: 암기왕, Time: 1632 ms, Memory: 243684 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.py
+++ 백준/Silver/2776. 암기왕/암기왕.py
@@ -1,38 +1,23 @@
+#딕셔너리를 이용해 탐색
 import sys
 input = sys.stdin.readline
 
 T = int(input().rstrip())
 
-#이진 탐색
-def binary_search(x, note):
-    start = 0
-    end = len(note) - 1 
-
-    while start <= end:
-        mid = (start + end) // 2
-
-        if note[mid] == x:
-            return mid
-        elif note[mid] > x:
-            end = mid - 1
-        elif note[mid] < x:
-            start = mid + 1
-    return -1
-
 for _ in range(T):
     N = int(input().rstrip())
     note1 = list(map(int, input().split()))
+    
+    #딕셔너리에 키값으로 저장
+    d = dict()
+    for i in note1:
+        d[i] = True
+
     M = int(input().rstrip())
     note2 = list(map(int, input().split()))
 
-    #이진 탐색을 하기 위한 정렬 과정 O(NlogN)
-    note1.sort()
-    #최대 백만 번 반복 - 이진탐색도 고려하면 O(NlogN)
     for i in note2:
-        result = binary_search(i, note1)
-
-        if result == -1:
-            print(0)
-        else:
+        if d.get(i) == True:
             print(1)
-    
+        else:
+            print(0)
