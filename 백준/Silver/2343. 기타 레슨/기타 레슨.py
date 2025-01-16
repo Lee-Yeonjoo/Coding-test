cb7a87e342e0ceba7bc1b6b860ab2336d0e93dfc
Yeonjoo Lee <121855519+Lee-Yeonjoo@users.noreply.github.com>
2025-01-16 20:18:51 +0900

    [Silver I] Title: 기타 레슨, Time: 544 ms, Memory: 42168 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.py
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.py
@@ -1,3 +1,4 @@
+#좀 다른 풀이 방법
 import sys
 input = sys.stdin.readline
 
@@ -9,22 +10,20 @@ end = sum(lectures) #가능한 블루레이의 크기 중 최대값
 
 while start <= end:
     mid = (start + end) // 2
-    #print(mid)
 
-    sum = 0
-    i = 0
-    #모든 강의의 길이를 순회하면서 차곡차곡 mid값을 넘지 않게 저장하고, 마지막 칸에는 나머지 강의 모두 저장
-    for j in range(len(lectures)):
-
-        sum += lectures[j]
-        if sum > mid:
-            i += 1  #영역이 증가함 
-            sum = lectures[j]
-            if i == M:  #영역의 개수를 넘음
-                start = mid + 1
-                break
-        if j == len(lectures) - 1:
-            end = mid - 1
-            break
-        
-print(end + 1)
+    sum = 0    #강의 시간의 총합
+    count = 1  #블루레이의 개수를 세기 위한 변수 
+    #강의 시간을 순회하면서 mid와 비교해 블루레이의 개수를 카운트
+    for lect in lectures:
+        if sum + lect > mid:
+            count += 1
+            sum = 0
+        sum += lect
+    
+    if count <= M:
+        answer = mid 
+        end = mid - 1
+    else:
+        start = mid + 1
+    
+print(answer)
