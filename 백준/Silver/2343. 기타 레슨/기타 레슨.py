a650eb13913d83494a208e89905fc36e13ff4cf6
Yeonjoo Lee <121855519+Lee-Yeonjoo@users.noreply.github.com>
2025-01-16 15:43:53 +0900

    [Silver I] Title: 기타 레슨, Time: 1092 ms, Memory: 42168 KB -BaekjoonHub

---
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.py
@@ -0,0 +1,41 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+lectures = list(map(int, input().split()))
+
+start = 1
+end = N * max(lectures) #가능한 블루레이의 크기 중 최대값
+
+while start <= end:
+    mid = (start + end) // 2
+    #print(mid)
+
+    sum = 0
+    i = 0
+    #모든 강의의 길이를 순회하면서 차곡차곡 mid값을 넘지 않게 저장하고, 마지막 칸에는 나머지 강의 모두 저장
+    for j in range(len(lectures)):
+        if lectures[j] > mid:
+            start = mid + 1
+            break
+
+        sum += lectures[j]
+        if sum > mid:
+            i += 1  #영역이 증가함 
+            sum = lectures[j]
+            if i == M:  #영역의 개수를 넘음
+                start = mid + 1
+                break
+            if sum > mid:
+                start = mid + 1
+                break
+            elif sum <= mid and j == len(lectures) - 1:
+                end = mid - 1
+                break
+        #가능한 경우 
+        else:
+            if j == len(lectures) - 1:
+                end = mid - 1
+                break
+
+print(end + 1)
