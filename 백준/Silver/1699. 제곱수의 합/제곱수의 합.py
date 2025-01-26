4c719a2a501682b115cb7b8287d9fc349d6fd65a
Yeonjoo Lee <121855519+Lee-Yeonjoo@users.noreply.github.com>
2025-01-26 15:21:18 +0900

    [Silver II] Title: 제곱수의 합, Time: 6224 ms, Memory: 33192 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
@@ -4,14 +4,14 @@ INF = 1e9
 
 N = int(input().rstrip())
 dp = [INF] * (N+1)
+dp[0] = 0
 dp[1] = 1  #항의 최소 개수
 for i in range(2, N+1):
     x = 1
     while i-(x**2) >= 0:
-        if i-(x**2) == 0:  #i가 제곱수인 경우 항은 1개
-            dp[i] = 1  
-        else:  #i가 제곱수가 아닌 경우 일일히 비교하여 최소값을 탐색 
-            dp[i] = min(dp[i], dp[i-(x**2)] + 1)
+        #i가 제곱수가 아닌 경우 일일히 비교하여 최소값을 탐색 
+        if dp[i] > dp[i-(x**2)] + 1:
+            dp[i] = dp[i-(x**2)] + 1  #min연산보다 더 빠르다.
         x += 1
 
 print(dp[N])
