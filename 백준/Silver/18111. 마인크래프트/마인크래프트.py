6ffa3e905220c7a84aa067678a5f514083d332c5
Yeonjoo Lee <121855519+Lee-Yeonjoo@users.noreply.github.com>
2025-02-01 19:57:29 +0900

    [Silver II] Title: 마인크래프트, Time: 132 ms, Memory: 35492 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.py
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -1,20 +1,13 @@
 import sys
+from collections import Counter
 input = sys.stdin.readline
 INF = 1e9
 
 N, M, B = map(int, input().split())
-ground = []
-min_height = INF
-max_height = 0
-for _ in range(N):
-    x = list(map(int, input().split()))
-    ground.append(x)
-    min_x = min(x)
-    max_x = max(x)
-    if min_height > min_x:
-        min_height = min_x
-    if max_height < max_x:
-        max_height = max_x
+ground = [int(x) for _ in range(N) for x in input().split()]  #Counter함수를 쓰기 위해 1차원 배열로 입력받기
+height_count = Counter(ground)  #각 높이의 개수를 세줌
+min_height = min(height_count)
+max_height = max(height_count)
 
 time = INF
 height = 0
@@ -23,23 +16,17 @@ for h in range(min_height, max_height+1):
     block = B
     install = 0
     remove = 0
-    #깎는 과정을 먼저하여 블록을 확보 
-    for i in range(N):
-        for j in range(M):
-            x = ground[i][j] - h
-            if x == 0:
-                continue
-            
-            #몇 번 쌓아야 하는지 카운트
-            if x < 0:
-                install += abs(x)
-
-            #몇 번 깎아야 하는지 카운트
-            if x > 0:
-                remove += x
+    for curr_h, curr_h_count in height_count.items():
+        #블럭을 설치해야하는 경우
+        if h > curr_h:
+            install += (h - curr_h) * curr_h_count  #Counter를 통해 같은 높이에 해당하는 칸이 여러 개면 한번에 처리하니까 시간 단축 가능
+        
+        #블럭을 제거해야하는 경우
+        if h < curr_h:
+            remove += (curr_h - h) * curr_h_count
     
     block += remove  #깎은 만큼 블록의 수 증가
-    count += 2*remove
+    count += 2*remove  #2초니까 2곱하기
     if install <= block:
         count += install
     else:  #설치할 블록의 수가 모자라므로 continue
