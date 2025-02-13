a6d181ad6bdd0855a52db60b359d465e646297d1
Yeonjoo Lee <121855519+Lee-Yeonjoo@users.noreply.github.com>
2025-02-13 17:14:48 +0900

    [Silver I] Title: 신입 사원, Time: 3424 ms, Memory: 47036 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -1,5 +1,4 @@
 import sys
-import heapq
 input = sys.stdin.readline
 
 T = int(input().rstrip())
@@ -14,14 +13,13 @@ for _ in range(T):
     
     #지원자들의 서류 성적을 기준으로 정렬 
     applicants.sort()
-    q = []  #합격자 리스트
+    answer = 1  #서류 1등은 무조건 합격
+    top = applicants[0][1]  #서류 1등의 면접 순위로 초기화 
 
-    #서류 성적이 가장 높은 지원자는 바로 합격이니까 큐에 push 
-    heapq.heappush(q, applicants[0][1])
-
-    #정렬된 순으로 합격자 큐의 요소들 중 가장 면접 순위가 높은 지원자와 비교해서 순위가 더 높다면 push 
+    #정렬된 순으로 가장 높은 면접 순위와 비교해서 순위가 더 높으면 카운트
     for i in range(1, N):
-        if applicants[i][1] < q[0]:
-            heapq.heappush(q, applicants[i][1])
+        if applicants[i][1] < top:
+            answer += 1
+            top = applicants[i][1]
 
-    print(len(q))        
+    print(answer)        
