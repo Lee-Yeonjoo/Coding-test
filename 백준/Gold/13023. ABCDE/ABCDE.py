8b444fc0ba86df93bb3f349dbc456e947a4e01c8
Yeonjoo Lee <121855519+Lee-Yeonjoo@users.noreply.github.com>
2025-02-19 15:31:03 +0900

    [Gold V] Title: ABCDE, Time: 1452 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -0,0 +1,39 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+friends = [[] for _ in range(N)]
+for _ in range(M):
+    a, b = map(int, input().split())
+    friends[a].append(b)
+    friends[b].append(a)
+
+def dfs(x, count):
+    global check
+
+    #dfs 깊이가 4이상이면 조건에 해당 
+    if count >= 4:
+        check = True
+        return
+
+    visited[x] = True
+
+    for i in friends[x]:
+        if visited[i] == False:
+            #깊이를 증가시킨다. 
+            dfs(i, count + 1)
+            visited[i] = False  #노드 번호가 작은게 먼저 방문 처리되는데, 그러면 답을 못 찾을 수 있으니 백트래킹으로 방문처리를 취소해야함!! 
+
+#모든 노드에서 dfs를 실행해 깊이를 측정            
+for i in range(N):
+    visited = [False] * N
+    check = False
+    dfs(i, 0)
+
+    if check == True:
+        print(1)
+        sys.exit()
+
+print(0)
