6d4d1e0577621dbe7ff1a9b824a56f22f8cf8a6f
Yeonjoo Lee <121855519+Lee-Yeonjoo@users.noreply.github.com>
2025-02-27 14:29:56 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 60 ms, Memory: 34968 KB -BaekjoonHub

---
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -0,0 +1,47 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
+    A, B = map(int, input().split())
+
+    #친구관계가 중복되어 들어올 수도 있다는 문제 조건 때문에 중복 저장 방지 
+    if B in graph[A]:
+        continue
+
+    graph[A].append(B)
+    graph[B].append(A)
+
+#bfs로 탐색하여 관계 수를 count배열에 저장 
+def bfs(x):
+    q = deque()
+    visited = [False] * (N+1)
+    count = [0] * (N+1)
+
+    q.append(x)
+    visited[x] = True
+
+    while q:
+        v = q.popleft()
+        
+        for i in graph[v]:
+            if visited[i] == False:
+                q.append(i)
+                visited[i] = True
+                count[i] = count[v] + 1 
+
+    return sum(count)
+
+result = []
+#모든 노드를 시작점으로 bfs
+for i in range(1, N+1):
+    result.append(bfs(i))
+
+answer = min(result)
+for i in range(N):
+    if result[i] == answer:
+        print(i+1)
+        break
