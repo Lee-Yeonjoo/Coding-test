466759002bd6baf176ca29fecaf7a417c445d226
Yeonjoo Lee <121855519+Lee-Yeonjoo@users.noreply.github.com>
2025-03-07 03:14:35 +0900

    [Silver II] Title: 이동하기, Time: 676 ms, Memory: 71692 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+maze = [list(map(int, input().split())) for _ in range(N)]
+
+#맨 윗줄과 왼쪽 줄을 먼저 초기화 
+for i in range(1, M):
+    maze[0][i] += maze[0][i-1]
+
+for i in range(1, N):
+    maze[i][0] += maze[i-1][0]
+
+#나머지에 대해 각 경우의 최댓값을 고른 후 자기 자신과 더한다.
+for i in range(1, N):
+    for j in range(1, M):
+        maze[i][j] += max(maze[i-1][j], max(maze[i][j-1], maze[i-1][j-1]))
+    
+print(maze[N-1][M-1])
