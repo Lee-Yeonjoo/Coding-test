262017b71104b6eef4a0fa86ab503f9f2770c976
Yeonjoo Lee <121855519+Lee-Yeonjoo@users.noreply.github.com>
2025-03-12 15:51:01 +0900

    [Silver I] Title: 스타트링크, Time: 412 ms, Memory: 41944 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.py
+++ 백준/Silver/5014. 스타트링크/스타트링크.py
@@ -4,35 +4,30 @@ input = sys.stdin.readline
 
 F, S, G, U, D = map(int, input().split())
 
-graph = [[] for _ in range(F+1)]
-
-#그래프 생성 - 범위 내라면 각 노드i에 i+U번 노드와 i-D번 노드를 연결(방향 존재).
-for i in range(1, F+1):
-    if i + U <= F:
-        graph[i].append(i + U)
-    if i - D >= 1:
-        graph[i].append(i - D)
-
 visited = [False] * (F+1)
-cost = [-1] * (F+1)
-#bfs로 S노드에서 시작해 각 노드에 대한 최단경로를 구한다. 
-def bfs(x):
+#bfs로 S노드에서 시작해 각 노드에 대한 최단경로를 구한다.
+#각 노드의 이웃노드는 1개 or 2개여서 굳이 그래프로  나타낼 필요없음!  
+def bfs(s, g):
     q = deque()
-    q.append(x)
-    visited[x] = True
-    cost[x] = 0
+    q.append((s, 0))  #큐에 노드와 cost를 추가 
+    visited[s] = True
 
     while q:
-        v = q.popleft()
+        v, cost = q.popleft()
+        
+        #이 조건문은 시작노드가 바로 도착지일 수 있기 때문에 이웃을 찾는 for문 안에 넣으면 안된다. 
+        if v == g:
+            return cost
+        
+        #그래프로 나타내기 대신 바로 이웃노드를 탐색 가능 
+        for adj in (v+U, v-D):    
+            if adj < 1 or adj > F: 
+                continue
 
-        for adj in graph[v]:
             if visited[adj] == False:
-                q.append(adj)
-                cost[adj] = cost[v] + 1
+                q.append((adj, cost + 1))
                 visited[adj] = True
+    #while문이 끝난거면 해당 노드로 갈 수 없는 거임 
+    return "use the stairs"
 
-bfs(S)
-if cost[G] == -1:
-    print("use the stairs")
-else:
-    print(cost[G])
+print(bfs(S, G))
