8987544641392f2711f611ed09df34663d13bc46
Yeonjoo Lee <121855519+Lee-Yeonjoo@users.noreply.github.com>
2025-03-14 21:45:12 +0900

    [Silver III] Title: 전쟁 - 땅따먹기, Time: 9728 ms, Memory: 52528 KB -BaekjoonHub

--- 백준/Silver/1270. 전쟁 － 땅따먹기/전쟁 － 땅따먹기.py
+++ 백준/Silver/1270. 전쟁 － 땅따먹기/전쟁 － 땅따먹기.py
@@ -10,10 +10,10 @@ for i in range(n):
     answer = -1
     for j in soldier[1::]:
         #이미 딕셔너리에 저장된 번호라면 카운트 증가 
-        if j in count.keys():
+        if j in count:
             count[j] += 1
             #만약 과반수 초과라면 답이므로 break 
-            if count[j] > soldier[0]//2:
+            if count[j] > soldier[0]/2:
                 answer = j
                 break
         #딕셔너리에 아직 저장 안한 번호이므로 새로 저장 
