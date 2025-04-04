e17262d0e82e7ffcd21d5c401dd5504f8b74262d
Yeonjoo Lee <121855519+Lee-Yeonjoo@users.noreply.github.com>
2025-04-04 17:28:46 +0900

    [Silver I] Title: 컴백홈, Time: 140 ms, Memory: 24556 KB -BaekjoonHub

---
+++ 백준/Silver/1189. 컴백홈/컴백홈.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int answer = 0;  //값 유지하기 위해 static 변수
+
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int R = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+
+        int[][] visited = new int[R][C];
+        for (int i = 0; i < R; i++) {
+            String x = br.readLine();
+            for (int j = 0; j < C; j++) {
+                //못 가는 부분 표시
+                if (x.charAt(j) == 'T') {
+                    visited[i][j] = -1;
+                }
+            }
+        }
+
+        dfs(R - 1, 0, visited, 1, R, C, K);
+        System.out.println(answer);
+    }
+
+    public static void dfs(int i, int j, int[][] visited, int k, int R, int C, int K) {
+        //도착했고, 거리도 K와 같다면 조건을 만족하므로 answer 카운트
+        if (i == 0 && j == C - 1 && k == K) {
+            answer++;
+            return;
+        }
+
+        visited[i][j] = 1;  //방문 표시
+
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+
+        //이웃 탐색
+        for (int s = 0; s < 4; s++) {
+            int nx = i + dx[s];
+            int ny = j + dy[s];
+
+            if (nx < 0 || nx >= R || ny < 0 || ny >= C) {
+                continue;
+            }
+
+            if (visited[nx][ny] == 0) {
+                dfs(nx, ny, visited, k+1, R, C, K);
+                //백트래킹
+                visited[nx][ny] = 0;
+            }
+        }
+    }
+}
