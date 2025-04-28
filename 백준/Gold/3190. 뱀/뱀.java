5e9a3e2fb9da139220d1117dc52415b30986abed
Yeonjoo Lee <121855519+Lee-Yeonjoo@users.noreply.github.com>
2025-04-29 03:41:37 +0900

    [Gold IV] Title: 뱀, Time: 112 ms, Memory: 14860 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.java
+++ 백준/Gold/3190. 뱀/뱀.java
@@ -2,14 +2,12 @@ import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
     static int x = 0, y = 0;
-    
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,73 +22,32 @@ public class Main {
         }
 
         //자바의 Point타입을 이용
-        Queue<Point> snake = new LinkedList<>();  //큐에 뱀이 차지한 좌표를 저장 
+        Queue<Point> snake = new LinkedList<>();  //큐에 뱀이 차지한 좌표를 저장
         snake.offer(new Point(0, 0));
         double direction = 0;  //뱀의 진행 방향 저장 - 각도로 저장
         int time = 0;  //시간 저장
 
         int nx, ny, dx = 0, dy = 0;
         int L = Integer.parseInt(br.readLine());
-        //입력 방향에 대해 반복
+        //방향 전환을 해시맵에 저장
+        Map<Integer, String> changeDirection = new HashMap<>();
         for (int i = 0; i < L; i++) {
+
             st = new StringTokenizer(br.readLine());
             int X = Integer.parseInt(st.nextToken());
             String S = st.nextToken();
 
-            //진행 좌표 구하기
-            dx = -1 * (int) Math.sin(Math.toRadians(direction));
-            dy = (int) Math.cos(Math.toRadians(direction));
-
-
-            //시간이 X초가 될 때까지 반복
-            while (time < X) {
-
-                nx = x + dx;
-                ny = y + dy;
-
-                //큐에 추가하기 전에 몸에 닿는지 검사 먼저
-                if (snake.contains(new Point(nx, ny))) {
-                    System.out.println(time + 1);
-                    return;
-                }
-
-                //만약 벽에 닿은 경우 종료
-                if ((nx < 0 || nx >= N) || (ny < 0 || ny >= N)) {
-                    System.out.println(time + 1);
-                    return;
-                }
-
-                //큐에 새로 이동한 좌표를 추가
-                snake.add(new Point(nx, ny));
-
-                //이동한 곳에 사과가 없으면 꼬리 제거
-                if (!isApple[nx][ny]) {
-                    snake.poll();
-                }
-                else isApple[nx][ny] = false;  //사과가 있으면 치워야한다!
-
-                time++;  //시간 증가
-                //이전 좌표 기록
-                x = nx;
-                y = ny;
-            }
-
-            //방향 전환
-            //오른쪽으로 회전
-            if (S.equals("D")) {
-                direction = (direction + 270) % 360;
-            } else direction = (direction + 90) % 360;  //왼쪽으로 회전
+            changeDirection.put(X, S); 
         }
-
-        //회전 없이
-        dx = -1 * (int) Math.sin(Math.toRadians(direction));
-        dy = (int) Math.cos(Math.toRadians(direction));
+        
         while (true) {
-            nx = x + dx;
-            ny = y + dy;
 
-            //큐에 추가하기 전에 몸에 닿는지 검사 먼저
-            if (snake.contains(new Point(nx, ny))) {
+            //이동할 좌표 구하기 - direction에 저장된 각도에 따른 -sin값, cos값으로 진행 방향 결정
+            nx = x + -1 * (int) Math.sin(Math.toRadians(direction));
+            ny = y + (int) Math.cos(Math.toRadians(direction));
+
+            //큐에 추가하기 전에 몸에 닿는지 검사
+            if (snake.contains(new Point(nx, ny))) {  //자바의 Point타입을 쓰면 contains를 그냥 써도 비교 가능! 다른 타입은 equals(), hashCode()를 오버라이드 해야한다.
                 System.out.println(time + 1);
                 return;
             }
@@ -101,17 +58,26 @@ public class Main {
                 return;
             }
 
+            //큐에 새로 이동한 좌표를 추가
             snake.add(new Point(nx, ny));
 
             //이동한 곳에 사과가 없으면 꼬리 제거
             if (!isApple[nx][ny]) {
                 snake.poll();
-            }
+            } else isApple[nx][ny] = false;  //사과가 있으면 치워야한다!
 
             time++;  //시간 증가
             //이전 좌표 기록
             x = nx;
             y = ny;
+
+            //방향 전환 - 해시에 현재 시간에 대한 방향 전환이 존재하는 경우
+            if (changeDirection.containsKey(time)) {
+                //오른쪽으로 회전
+                if (changeDirection.get(time).equals("D")) {
+                    direction = (direction + 270) % 360;
+                } else direction = (direction + 90) % 360;  //왼쪽으로 회전
+            }
         }
     }
 }
