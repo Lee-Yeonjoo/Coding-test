c48115aedf974753064404f4b38bf6b9eaf7a088
Yeonjoo Lee <121855519+Lee-Yeonjoo@users.noreply.github.com>
2025-05-08 22:24:57 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 136 ms, Memory: 14804 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -14,7 +14,7 @@ public class Main {
             sequence[i] = Integer.parseInt(st.nextToken());
         }
 
-        //0열은 가장 긴 증가하는 부분 수열의 크기를 저장, 1열은 가장 긴 감소(or 증가했다 감소)하는 부분 수열의 크기를 저장
+        //0열은 가장 긴 증가하는 부분 수열의 크기를 저장, 1열은 가장 긴 감소(or 증가했다 감소 중)하는 부분 수열의 크기를 저장
         int[][] dp = new int[N][2];
         //자기 자신을 포함하는 경우를 저장하기 때문에 최소 길이는 1 -> 1로 초기화
         for (int i = 0; i < N; i++) {
@@ -24,12 +24,11 @@ public class Main {
         int maxLength = 1;  //최소 1은 보장되므로 1로 초기화
         for (int i = 1; i < N; i++) {
             for (int j = 0; j < i; j++) {
-                //증가하는 부분 수열인 경우
+                //증가 중인 경우
                 if (sequence[i] > sequence[j]) {
                     dp[i][0] = Math.max(dp[i][0], dp[j][0] + 1);
-                } else if (sequence[i] < sequence[j]) {
-                    dp[i][1] = Math.max(dp[i][1], dp[j][1] + 1);
-                    dp[i][1] = Math.max(dp[i][1], dp[j][0] + 1);  //증가했다 감소하는 경우
+                } else if (sequence[i] < sequence[j]) {  //증가했다 이제 감소하는 경우 vs 감소 중인 경우
+                    dp[i][1] = Math.max(dp[i][1], Math.max(dp[j][0], dp[j][1]) + 1);
                 }
 
                 maxLength = Math.max(maxLength, Math.max(dp[i][0], dp[i][1]));
