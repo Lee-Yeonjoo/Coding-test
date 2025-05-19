6d0f080baae9e15e54b9a6c9ba15cc97070d2405
Yeonjoo Lee <121855519+Lee-Yeonjoo@users.noreply.github.com>
2025-05-19 21:49:26 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 480 ms, Memory: 18100 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
@@ -1,46 +1,57 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Objects;
 
 public class Main {
-    static int[][] tree;
+    static StringBuilder sb = new StringBuilder();
+    static class Node {
+        int num;
+        Node lChild;
+        Node rChild;
+
+        public Node(int num, Node lChild, Node rChild) {
+            this.num = num;
+            this.lChild = lChild;
+            this.rChild = rChild;
+        }
+
+        //트리에 n을 후손으로 추가
+        void insert(int n) {
+            if (n < this.num) {
+                if (this.lChild == null) {
+                    this.lChild = new Node(n, null, null);
+                } else {
+                    this.lChild.insert(n);
+                }
+            } else {
+                if (this.rChild == null) {
+                    this.rChild = new Node(n, null, null);
+                } else {
+                    this.rChild.insert(n);
+                }
+            }
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int root = Integer.parseInt(br.readLine());
-        tree = new int[1000001][2];
+        //처음 입력받는 수는 무조건 루트 노드
+        Node root = new Node(Integer.parseInt(br.readLine()), null, null);
         String input;
         while ((input = br.readLine()) != null) {
-            int x = Integer.parseInt(input);
-
-            int i = root;
-            int j = root > x? 0 : 1;
-            while (tree[i][j] != 0) {
-                if (x < i) {
-                    i = tree[i][0];
-                } else {
-                    i = tree[i][1];
-                }
-                j = i > x? 0 : 1;
-            }
-            tree[i][j] = x;
+            root.insert(Integer.parseInt(input));
         }
+
         postOrderTraverse(root);
+        System.out.println(sb);
     }
 
-    //후위 순회 함수
-    static void postOrderTraverse(int num) {
-        int leftChild = tree[num][0];
-        int rightChild = tree[num][1];
-        //왼쪽 자식이 있다면 왼쪽 자식을 루트로 하는 서브트리에 대해 후위 순회
-        if (leftChild != 0) {
-            postOrderTraverse(leftChild);
+    //후위 순회
+    static void postOrderTraverse(Node node) {
+        if (node == null) {
+            return;
         }
-        //오른쪽 자식이 있다면 오른쪽 서브트리 후위 순회
-        if (rightChild != 0) {
-            postOrderTraverse(rightChild);
-        }
-        //자식이 없거나, 서브트리의 순회가 끝나면 자기 자신 출력
-        System.out.println(num);
+        postOrderTraverse(node.lChild);
+        postOrderTraverse(node.rChild);
+        sb.append(node.num).append("\n");
     }
 }
