0ee5509fca91d56925ea9b6cdd119d0ec24c8c03
Yeonjoo Lee <121855519+Lee-Yeonjoo@users.noreply.github.com>
2025-06-23 04:33:01 +0900

    [Gold V] Title: A와 B, Time: 112 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.java
+++ 백준/Gold/12904. A와 B/A와 B.java
@@ -2,32 +2,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-//구현, 왜 그리디?
+//구현, 왜 그리디
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String S = br.readLine();
-        String T = br.readLine();
+        //StringBuilder를 사용하면 쉽게 문자를 제거하고, 뒤집을 수 있다.
+        StringBuilder S = new StringBuilder(br.readLine());
+        StringBuilder T = new StringBuilder(br.readLine());
 
-        while (!T.isEmpty()) {
+        //T의 길이가 클 때까지만 반복 -> T의 길이는 줄어든다.
+        while (T.length() > S.length()) {
             char last = T.charAt(T.length() - 1);  //마지막 문자 추출
-            T = T.substring(0, T.length() - 1);  //마지막 문자 제거
+            T.deleteCharAt(T.length() - 1);  //마지막 문자 제거
 
             //마지막 문자가 B인 경우 뒤집기
-            String temp = "";
             if (last == 'B') {
-                for (int i = T.length() - 1; i >= 0; i--) {
-                    temp += T.charAt(i);
-                }
-            } else temp = T;
-
-            //만약 연산 결과가 S와 동일하다면 1 출력 후 종료
-            if (temp.equals(S)) {
-                System.out.println(1);
-                System.exit(0);
+                T.reverse();
             }
-            T = temp;  //다시 T에 저장
         }
-        System.out.println(0);  //불가능한 경우 0 출력
+
+        //S와 T가 동일하다면 1 출력, 아니면 0 출력
+        System.out.println(T.compareTo(S) == 0? 1 : 0);
     }
 }
