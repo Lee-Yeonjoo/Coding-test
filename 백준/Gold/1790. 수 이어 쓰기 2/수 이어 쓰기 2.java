c94883c002ab8641916fb85ef6c9e5397a624a30
Yeonjoo Lee <121855519+Lee-Yeonjoo@users.noreply.github.com>
2025-07-28 17:27:35 +0900

    [Gold V] Title: 수 이어 쓰기 2, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Gold/1790. 수 이어 쓰기 2/수 이어 쓰기 2.java
+++ 백준/Gold/1790. 수 이어 쓰기 2/수 이어 쓰기 2.java
@@ -9,35 +9,25 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
+        long k = Integer.parseInt(st.nextToken());
 
-        int[] count = new int[10]; // 자리수별 숫자 개수 저장
-        long len = 0;
-
-        for (int i = 1; i < 10; i++) {
-            int start = (int) Math.pow(10, i - 1);
-            int end = (int) Math.min(N, Math.pow(10, i) - 1);
-
-            if (start > N) break;
-
-            count[i] = end - start + 1;
-            len += (long) count[i] * i;
+        int sitNum = 1;  //자릿수
+        long nines = 9;  //9, 90, 900...을 담을 변수 -> 자릿수에 따른 범위의 숫자 개수
+        while (k > nines * sitNum) {  //자릿수를 곱해야 숫자 하나씩 자랐을 때의 개수임
+            k -= nines * sitNum;
+            nines *= 10;
+            sitNum++;
         }
+        k--;  //숫자 조정
 
-        if (len < k) {
+        //k번째 수를 포함하는 숫자 찾기
+        long num = (long) Math.pow(10, sitNum - 1) + (k / sitNum);  //k가 10억일 때 sitNum도 커져서 10^(sitNum)이 커질 수 있기 때문에 long타입
+
+        //예외처리
+        if (num > N) {
             System.out.println(-1);
-            return;
+        } else {
+            System.out.println(String.valueOf(num).charAt((int) (k % sitNum)));
         }
-
-        int i = 1;
-        while (k > count[i] * i) {
-            k -= count[i] * i;
-            i++;
-        }
-
-        int q = (k - 1) / i;
-        int r = (k - 1) % i;
-        int num = (int) Math.pow(10, i - 1) + q;
-        System.out.println(Integer.toString(num).charAt(r));
     }
 }
