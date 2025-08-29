f0b4c2f231488f58245a209acd0359fa4bc39283
Yeonjoo Lee <121855519+Lee-Yeonjoo@users.noreply.github.com>
2025-08-29 16:07:10 +0900

    [Gold IV] Title: 주난의 난(難), Time: 372 ms, Memory: 109356 KB -BaekjoonHub

--- 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.java
+++ 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.java
@@ -2,6 +2,8 @@ import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.Array;
+import java.util.ArrayDeque;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -24,10 +26,7 @@ public class Main {
         chocoY = Integer.parseInt(st.nextToken());
         classInfo = new char[N][M];
         for (int i = 0; i < N; i++) {
-            String input = br.readLine();
-            for (int j = 0; j < M; j++) {
-                classInfo[i][j] = input.charAt(j);
-            }
+            classInfo[i] = br.readLine().toCharArray();  //간단하게 문자 배열로 변환
         }
 
         //초콜릿 도둑을 찾을 때까지 bfs반복
@@ -41,17 +40,17 @@ public class Main {
     static void bfs() {
         int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, -1, 1};
-        Queue<Point> q = new LinkedList<>();
-        q.add(new Point(junanX, junanY));
+        Queue<int[]> q = new ArrayDeque<>();  //LinkedList보다 ArrayDeque가 bfs에 더 적합
+        q.add(new int[]{junanX, junanY});     //Point클래스는 GUI 관련 클래스라 Node클래스를 따로 만들거나, int배열 쓰는게 더 빠르다
         boolean[][] visited = new boolean[N][M];
         visited[junanX][junanY] = true;
 
         while (!q.isEmpty()) {
-            Point curr = q.poll();
+            int[] curr = q.poll();
 
             for (int i = 0; i < 4; i++) {
-                int nx = curr.x + dx[i];
-                int ny = curr.y + dy[i];
+                int nx = curr[0] + dx[i];
+                int ny = curr[1] + dy[i];
 
                 if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
                     continue;
@@ -61,11 +60,12 @@ public class Main {
                     visited[nx][ny] = true;
                     //0인 경우에만 계속 탐색
                     if (classInfo[nx][ny] == '0') {
-                        q.add(new Point(nx, ny));
+                        q.add(new int[]{nx, ny});
                     }
                     //초코 도둑 찾은 경우
                     else if (classInfo[nx][ny] == '#') {
                         isChoco = true;
+                        return;
                     } else {  //1인 경우 0으로
                         classInfo[nx][ny] = '0';
                     }
