28efdfd35ef1907346227ce102edab24351a9535
Yeonjoo Lee <121855519+Lee-Yeonjoo@users.noreply.github.com>
2025-09-03 02:18:54 +0900

    [Gold V] Title: 달력, Time: 128 ms, Memory: 14596 KB -BaekjoonHub

--- 백준/Gold/20207. 달력/README.md
+++ 백준/Gold/20207. 달력/README.md
@@ -4,7 +4,7 @@
 
 ### 성능 요약
 
-메모리: 14988 KB, 시간: 136 ms
+메모리: 14596 KB, 시간: 128 ms
 
 ### 분류
 
@@ -12,7 +12,7 @@
 
 ### 제출 일자
 
-2025년 9월 3일 01:39:30
+2025년 9월 3일 02:18:35
 
 ### 문제 설명
 
@@ -37,11 +37,11 @@
 	<li>하루의 폭은 1이다. </li>
 </ul>
 
-<p style="text-align: center;"><img alt="" src="" style="width: 720px; height: 225px;"></p>
+<p style="text-align: center;"><img alt="" src="https://upload.acmicpc.net/1a820e79-e5fc-4e4a-b7ad-efe42cfd7cdd/" style="width: 720px; height: 225px;"></p>
 
 <p>위의 그림에서와 같이 일정이 주어졌다고 하자. 여기서 코팅지의 면적은 아래의 파란색 영역과 같다.</p>
 
-<p style="text-align: center;"><img alt="" src="" style="width: 720px; height: 224px;"></p>
+<p style="text-align: center;"><img alt="" src="https://upload.acmicpc.net/680c1b8a-7ae1-4b00-ba41-e1c61cd64846/" style="width: 720px; height: 224px;"></p>
 
 <p>이때 코팅지의 크기의 합은 3 x 8 + 2 x 2 = 28이다. </p>
 
--- 백준/Gold/20207. 달력/달력.java
+++ 백준/Gold/20207. 달력/달력.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-//그리디, 구현 아이디어가 어려웠드아
+//그리디, 구현 아이디어가 어려웠드아/ 더 간단한 풀이
 public class Main {
     static class Schedule implements Comparable<Schedule>{
         int start;
@@ -35,8 +35,7 @@ public class Main {
         //정렬 - 1. 시작일(오름차순) 2. 종료일(내림차순)
         Collections.sort(scheduleList);
 
-        //세로 길이 구하기 위해 일자별로 카운트
-        int height = 0;
+        //일자별로 일정 카운트
         int[] dayCount = new int[366];
         for (Schedule schedule : scheduleList) {
             for (int i = schedule.start; i <= schedule.end; i++) {
@@ -44,30 +43,22 @@ public class Main {
             }
         }
 
-        //가로 길이
-        int widthStart = scheduleList.get(0).start;
-        int widthEnd = scheduleList.get(0).end;
+        //면적 구하기
         int area = 0;
-        for (int i = 1; i < N; i++) {
-            if (scheduleList.get(i).start <= widthEnd + 1) {  //문제 잘 못 이해해서 틀림ㅠ widthEnd와 같거나 작은게 아니라 widthEnd+1보다 작거나 같아야 연속된 일정임
-                widthEnd = Math.max(widthEnd, scheduleList.get(i).end);
-            } else {
-                //세로 길이 구하기
-                for (int j = widthStart; j <= widthEnd; j++) {
-                    height = Math.max(height, dayCount[j]);
-                }
-                area += (widthEnd - widthStart + 1) * height;  //면적 더하기
-                //초기화 -> 다음 일정 그룹
-                widthStart = scheduleList.get(i).start;
-                widthEnd = scheduleList.get(i).end;
+        int width = 0;
+        int height = 0;
+        for (int i = 1; i < dayCount.length; i++) {
+            if (dayCount[i] == 0) {
+                area += width * height;
+                width = 0;
                 height = 0;
+            } else {
+                width++;
+                height = Math.max(height, dayCount[i]);
             }
         }
-        //마지막 일정 그룹 면적 구하고 합하기
-        for (int j = widthStart; j <= widthEnd; j++) {
-            height = Math.max(height, dayCount[j]);
-        }
-        area += (widthEnd - widthStart + 1) * height;
+        //마지막 일정 면적 더하기
+        area += width * height;
         System.out.println(area);
     }
 }
