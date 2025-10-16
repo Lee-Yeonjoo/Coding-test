ce59028e0c57170b3047d2ae78367abb05710513
Yeonjoo Lee <121855519+Lee-Yeonjoo@users.noreply.github.com>
2025-10-16 19:28:05 +0900

    [Gold V] Title: 동전 2, Time: 124 ms, Memory: 14400 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -6,47 +6,30 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-
         int[] coins = new int[n];
         for (int i = 0; i < n; i++) {
             coins[i] = Integer.parseInt(br.readLine());
         }
-        Arrays.sort(coins);  //동전의 가치를 오름차순 정렬
 
-        int[] dp = new int[k+1];
-        Arrays.fill(dp, 10001);  //최소값으로 갱신해야 하므로 큰 값으로 초기화
-        dp[0] = 0;  //0원이 되려면 동전의 개수는 0개
-
-        //바텀업 방식
+        int[] dp = new int[k + 1];  //합이 i가 되는 동전의 최소 개수
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;  //큰 수로 초기화하고, dp[0]은 0
         for (int i = 1; i < k + 1; i++) {
-            //가장 작은 가치의 동전보다 작은 가치는 만들 수 없으므로 continue
-            if (i < coins[0]) {
-                continue;
-            }
-
-            //각 동전의 가치들에 대해 탐색
-            for (int coin : coins) {
-                //i가 동전의 가치보다 작으므로 break
-                if (i < coin) {
-                    break;
+            //모든 동전에 대해 dp(i-동전값)으로 개수 구해서 최소값 갱신
+            for (int j = 0; j < n; j++) {
+                if (i >= coins[j] && dp[i - coins[j]] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
                 }
-
-                //i가 동전의 가치보다 크므로 최소값 구하기
-                dp[i] = Math.min(dp[i], dp[i - coin]);
             }
-
-            dp[i] += 1;
         }
-
-        if (dp[k] >= 10001) {
+        if (dp[k] == Integer.MAX_VALUE) {
             System.out.println(-1);
+        } else {
+            System.out.println(dp[k]);
         }
-        else System.out.println(dp[k]);
     }
 }
