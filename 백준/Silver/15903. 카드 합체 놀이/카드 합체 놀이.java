a128d2b52b880392179088df451603ed1b7f8958
Yeonjoo Lee <121855519+Lee-Yeonjoo@users.noreply.github.com>
2025-11-15 20:26:06 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 144 ms, Memory: 15252 KB -BaekjoonHub

--- 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.java
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.java
@@ -2,9 +2,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-//그리디 
+//그리디
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,25 +13,22 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
-        long[] cards = new long[n];  //int범위를 넘음
+        PriorityQueue<Long> pq = new PriorityQueue<>();
         for (int i = 0; i < n; i++) {
-            cards[i] = Long.parseLong(st.nextToken());
+            pq.add(Long.parseLong(st.nextToken()));
         }
 
         //카드 합치기
         for (int i = 0; i < m; i++) {
-            //오름차순 정렬
-            Arrays.sort(cards);
-
-            long sum = cards[0] + cards[1];
-            cards[0] = sum;
-            cards[1] = sum;
+            long sum = pq.poll() + pq.poll();
+            pq.add(sum);
+            pq.add(sum);
         }
 
         //최종 점수
         long answer = 0;
         for (int i = 0; i < n; i++) {
-            answer += cards[i];
+            answer += pq.poll();
         }
         System.out.println(answer);
     }
